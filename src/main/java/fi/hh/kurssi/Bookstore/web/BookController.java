9d890c888d4a76f6f63631cf54f82808df2eea7e
unknown <auri.ylen@myy.haaga-helia.fi>
2018-02-08 22:24:22 +0200

    Kirjojen listaus

--- src/main/java/fi/hh/kurssi/Bookstore/web/BookController.java
+++ src/main/java/fi/hh/kurssi/Bookstore/web/BookController.java
@@ -1,17 +1,30 @@
 package fi.hh.kurssi.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fi.hh.kurssi.Bookstore.domain.BookRepository;
+
 @Controller
-@ResponseBody
 public class BookController {
 	
+	@Autowired
+	private BookRepository repository;
+	
+	@ResponseBody
 	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public String welcomeBookstore(){
 		return "Tervetuloa kirjakauppaan!";
+	}
+	
+	@RequestMapping(value="/booklist", method=RequestMethod.GET)
+	public String listBooks(Model model){
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 	
 
