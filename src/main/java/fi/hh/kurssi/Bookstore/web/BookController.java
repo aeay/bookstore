aa5919dc9f28332ecd05057b3ec589dac62dffc5
unknown <auri.ylen@myy.haaga-helia.fi>
2018-02-09 00:02:45 +0200

    Poista, lisää ja Bootstrap

--- src/main/java/fi/hh/kurssi/Bookstore/web/BookController.java
+++ src/main/java/fi/hh/kurssi/Bookstore/web/BookController.java
@@ -3,10 +3,12 @@ package fi.hh.kurssi.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fi.hh.kurssi.Bookstore.domain.Book;
 import fi.hh.kurssi.Bookstore.domain.BookRepository;
 
 @Controller
@@ -26,6 +28,29 @@ public class BookController {
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
 	}
+	
+	 @RequestMapping(value = "/add")
+	    public String addBook(Model model){
+	    	model.addAttribute("book", new Book());
+	        return "addbook";
+	    }     
+	 
+	 @RequestMapping(value = "/save", method = RequestMethod.POST)
+	    public String save(Book book){
+	        repository.save(book);
+	        return "redirect:booklist";
+	    }    
+	 
+	 @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+	    	repository.delete(bookId);
+	        return "redirect:/booklist";
+	    }  
+	 
+	 @RequestMapping(value= "/edit/{id}", method=RequestMethod.GET)
+	 	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		 	repository.edit(bookId);
+	 }
 	
 
 }
