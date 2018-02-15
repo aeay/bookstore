52d292b921b1d05f6da850d8ec907867893b6618
unknown <auri.ylen@myy.haaga-helia.fi>
2018-02-15 11:40:54 +0200

    Kategorioiden lisäys

--- src/main/java/fi/hh/kurssi/Bookstore/domain/Category.java
+++ src/main/java/fi/hh/kurssi/Bookstore/domain/Category.java
@@ -13,60 +13,45 @@ public class Category {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long id;
+	private Long categoryid;
 	private String name;
 	
 	@OneToMany
 	private List<Book> books;
 	
-	
-	public List<Book> getBooks() {
-		return books;
-	}
-	public void setBooks(List<Book> books) {
-		this.books = books;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	@Override
-	public String toString() {
-		return "Category [id=" + id + ", name=" + name + ", books=" + books + "]";
-	}
-	
-	public Category(Long id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.books = null;
-	}
-	
-	public Category() {
-		super();
-		this.id = null;
-		this.name = null;
-		this.books = null;
-	}
+	public Category() {}
 	
 	public Category(String name) {
 		super();
-		this.id = null;
 		this.name = name;
-		this.books = null;
 	}
 	
+	public Long getCategoryid() {
+		return categoryid;
+	}
 	
+	public void setCategoryid(Long categoryid) {
+		this.categoryid = categoryid;
+	}
 	
+	public String getName() {
+		return name;
+	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [categoryid=" + categoryid + ", name=" + name + "]";
+	}
 }
